a991af005f9eca4a905ca8e572115679ad5c34ca
iboumatar <130079437+iboumatar@users.noreply.github.com>
2026-07-25 03:37:40 +0200

    pwa: add service worker

---
+++ public/sw.js
@@ -0,0 +1,25 @@
+
+const CACHE_NAME = "pondetrack-v1";
+const STATIC_ASSETS = ["/", "/index.html", "/static/js/main.chunk.js"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(STATIC_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
